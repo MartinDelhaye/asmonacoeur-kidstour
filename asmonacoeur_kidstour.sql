76c942b66d1e10c13e81641cd2e58898a2779fa9
ClaraMalaquin <cmalaquin4@gmail.com>
2024-12-16 10:19:26 +0100

    BDD
    changement BDD

--- asmonacoeur_kidstour.sql
+++ asmonacoeur_kidstour.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : dim. 15 déc. 2024 à 12:19
+-- Généré le : lun. 16 déc. 2024 à 09:18
 -- Version du serveur : 5.7.39
 -- Version de PHP : 8.2.0
 
@@ -30,14 +30,14 @@ SET time_zone = "+00:00";
 CREATE TABLE `anime` (
   `id_anime` int(11) NOT NULL,
   `id_invite` int(11) DEFAULT NULL,
-  `id_evenement` int(11) DEFAULT NULL
+  `id_etape` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `anime`
 --
 
-INSERT INTO `anime` (`id_anime`, `id_invite`, `id_evenement`) VALUES
+INSERT INTO `anime` (`id_anime`, `id_invite`, `id_etape`) VALUES
 (1, 1, 4),
 (2, 2, 1),
 (3, 3, 5),
@@ -67,11 +67,11 @@ CREATE TABLE `etapes` (
 --
 
 INSERT INTO `etapes` (`id_etape`, `date_etape`, `lieu_etape`, `nom_etape`, `description_etape`, `illustration_etape`, `image_etape`, `ville`, `heure`) VALUES
-(1, '2023-11-03', '2 Pl. de la République, 83440 Fayence', 'Le Kids Tour et Aleksandr Golovin à Fayence ce vendredi !', '\"Rendez-vous à la mairie.\r\nPour cette étape de l’AS Monaco Kids tour, la journée s’annonce riche en activités : \r\nRencontre avec notre mascotte Bouba et un invité surprise\r\nCible géante gonflable\r\nQuiz sur l’AS Monaco\r\nImmersion du Stade Louis-II grâce à des casques RV\r\nParties de FC 24 sur Playstation 5\r\nEt de belles surprises, telles qu’un maillot dédicacé de notre invité et des invitations pour des matchs sont à gagner ! Tout comme les goodies offerts, c’est une journée à ne pas manquer!\"', 'asmonacoeur-kidstour/images/Temp_kidstour_Complet.jpg', 'asmonacoeur-kidstour/images/Temp_kidstour_img_enfant1.jpg', 'Fayence', '13h30 - 17h30'),
-(2, '2023-11-17', '62 Av. du Trois Septembre, 06320 Cap-d\'Ail', 'Cap-d’Ail en mode Kids Tour !', '\"Rendez-vous à la mairie.\r\nPour cette étape de l’AS Monaco Kids tour, cette journée ensoleillée annonce pleins de surprises : \r\nRencontre avec notre mascotte et des invités surprises\r\nEntraînement de football\r\nQuiz sur l’AS Monaco\r\nParties de FC24 sur Playstation 5\r\nC’est aussi l’occasion de se mettre à l’épreuve lors d’un tire au but, les joueurs les plus précis auront le droit de repartir avec de belles surprises !\"', 'asmonacoeur-kidstour/images/Temp_kidstour_Cap_dAil.jpg', 'asmonacoeur-kidstour/images/Temp_kidstour_img_enfant2.jpg', 'Cap-d’Ail', '13h30 - 17h30'),
-(3, '2023-11-24', '1 Av. de la Victoire, 06320 La Turbie', 'Le Kids Tour à La Turbie vendredi 24 novembre !', '\"Rendez-vous sur la place de la mairie.\r\nPour cette étape notre équipe vous propose plusieurs divertissements : \r\nRencontre avec notre fidèle mascotte Bouba\r\nEntraînement de foot avec des joueurs de Monaco\r\nQuiz sur l’AS Monaco\r\nCréation de décoration de Noël\r\nLa caravane rouge et blanche sera toujours remplie de pleins de goodies à vous offrir, une autre journée à ne pas louper !\"', 'asmonacoeur-kidstour/images/Temp_kidstour_LaTurbine.jpg', 'asmonacoeur-kidstour/images/Temp_kidstour_img_enfant3.jpg', 'La Turbie', '13h30 - 17h30'),
-(4, '2023-12-01', '5 Pl. de la République, 06670 Levens', 'Avec Folarin Balogun, un Kids Tour à Levens !', '\"Rendez-vous sur la place de la mairie.\r\nPour cette étape, l’AS Monacoeur s’annonce encore une fois riche en surprises et émotions. De nombreux défis sont mis en place pour tenter de gagner les calendriers AS Monaco spécial Noël !\"', 'asmonacoeur-kidstour/images/Temp_kidstour_Fayences_Levens.jpg', 'asmonacoeur-kidstour/images/Temp_kidstour_img_enfant4.jpg', 'Levens', '13h30 - 17h30'),
-(5, '2023-12-08', 'Quai Albert 1er, 98000 Monaco', 'Le Kids Tour et Breel Embolo au Village de Noël de Monaco ce vendredi !', '\"Rendez-vous au village de Noël\r\nUne journée encore remplie de surprises avec des séances de dédicaces, une visite du marché et d’autres activités et cadeaux pour fêter ce Noël !\"', 'asmonacoeur-kidstour/images/Temp_kidstour_Monaco.jpg', 'asmonacoeur-kidstour/images/Temp_kidstour_img_enfant5.jpg', 'Monaco', '13h30 - 17h30');
+(1, '2023-11-03', '2 Pl. de la République, 83440 Fayence', 'Le Kids Tour et Aleksandr Golovin à Fayence ce vendredi !', '\"Rendez-vous à la mairie.\r\nPour cette étape de l’AS Monaco Kids tour, la journée s’annonce riche en activités : \r\nRencontre avec notre mascotte Bouba et un invité surprise\r\nCible géante gonflable\r\nQuiz sur l’AS Monaco\r\nImmersion du Stade Louis-II grâce à des casques RV\r\nParties de FC 24 sur Playstation 5\r\nEt de belles surprises, telles qu’un maillot dédicacé de notre invité et des invitations pour des matchs sont à gagner ! Tout comme les goodies offerts, c’est une journée à ne pas manquer!\"', 'images/Temp_kidstour_Complet.jpg', 'images/Temp_kidstour_img_enfant1.jpg', 'Fayence', '13h30 - 17h30'),
+(2, '2023-11-17', '62 Av. du Trois Septembre, 06320 Cap-d\'Ail', 'Cap-d’Ail en mode Kids Tour !', '\"Rendez-vous à la mairie.\r\nPour cette étape de l’AS Monaco Kids tour, cette journée ensoleillée annonce pleins de surprises : \r\nRencontre avec notre mascotte et des invités surprises\r\nEntraînement de football\r\nQuiz sur l’AS Monaco\r\nParties de FC24 sur Playstation 5\r\nC’est aussi l’occasion de se mettre à l’épreuve lors d’un tire au but, les joueurs les plus précis auront le droit de repartir avec de belles surprises !\"', 'images/Temp_kidstour_Cap_dAil.jpg', 'images/Temp_kidstour_img_enfant2.jpg', 'Cap-d’Ail', '13h30 - 17h30'),
+(3, '2023-11-24', '1 Av. de la Victoire, 06320 La Turbie', 'Le Kids Tour à La Turbie vendredi 24 novembre !', '\"Rendez-vous sur la place de la mairie.\r\nPour cette étape notre équipe vous propose plusieurs divertissements : \r\nRencontre avec notre fidèle mascotte Bouba\r\nEntraînement de foot avec des joueurs de Monaco\r\nQuiz sur l’AS Monaco\r\nCréation de décoration de Noël\r\nLa caravane rouge et blanche sera toujours remplie de pleins de goodies à vous offrir, une autre journée à ne pas louper !\"', 'images/Temp_kidstour_LaTurbine.jpg', 'images/Temp_kidstour_img_enfant3.jpg', 'La Turbie', '13h30 - 17h30'),
+(4, '2023-12-01', '5 Pl. de la République, 06670 Levens', 'Avec Folarin Balogun, un Kids Tour à Levens !', '\"Rendez-vous sur la place de la mairie.\r\nPour cette étape, l’AS Monacoeur s’annonce encore une fois riche en surprises et émotions. De nombreux défis sont mis en place pour tenter de gagner les calendriers AS Monaco spécial Noël !\"', 'images/Temp_kidstour_Fayences_Levens.jpg', 'images/Temp_kidstour_img_enfant4.jpg', 'Levens', '13h30 - 17h30'),
+(5, '2023-12-08', 'Quai Albert 1er, 98000 Monaco', 'Le Kids Tour et Breel Embolo au Village de Noël de Monaco ce vendredi !', '\"Rendez-vous au village de Noël\r\nUne journée encore remplie de surprises avec des séances de dédicaces, une visite du marché et d’autres activités et cadeaux pour fêter ce Noël !\"', 'images/Temp_kidstour_Monaco.jpg', 'images/Temp_kidstour_img_enfant5.jpg', 'Monaco', '13h30 - 17h30');
 
 -- --------------------------------------------------------
 
@@ -93,11 +93,11 @@ CREATE TABLE `invites` (
 --
 
 INSERT INTO `invites` (`id_invite`, `nom__invite`, `prenom__invite`, `description_invite`, `contact_invite`, `image_invite`) VALUES
-(1, 'Balogun', 'Folarin', 'Folarin Balogun est un jeune footballeur talentueux né le 3 juillet 2001 à New York, aux États-Unis. Il joue comme attaquant et est connu pour sa vitesse et ses nombreux buts. Balogun a grandi en Angleterre et a joué pour Arsenal avant de rejoindre l’AS Monaco en France. Il représente l’équipe nationale des États-Unis et est un espoir prometteur dans le monde du football. Avec son énergie et son talent, il inspire de nombreux jeunes à suivre leurs rêves dans le sport.', 'téléphone : 06 12 34 56 78', 'asmonacoeur-kidstour/images/Folarin_Balogun.jpg'),
-(2, 'Golovin', 'Aleksandr', 'Aleksandr Golovin est un footballeur russe né le 30 mai 1996 à Kaltan, en Russie. Il joue au milieu de terrain et est connu pour sa technique, sa vision de jeu et ses passes précises. Golovin a commencé sa carrière en Russie avant de rejoindre l’AS Monaco, un club de Ligue 1 en France. Il a également représenté l’équipe nationale de Russie dans plusieurs compétitions internationales. Son travail acharné et son style de jeu créatif en font un joueur très apprécié des fans de football.', 'téléphone : 07 98 76 54 32', 'asmonacoeur-kidstour/images/Aleksandr_Golovin.jpg'),
-(3, 'Minamino', 'Takumi', 'Takumi Minamino est un footballeur japonais né le 16 janvier 1995 à Izumisano, au Japon. Il joue comme attaquant ou milieu offensif et est connu pour sa rapidité, sa technique et son intelligence sur le terrain. Minamino a joué pour des clubs célèbres comme le Red Bull Salzburg et Liverpool avant de rejoindre l’AS Monaco en France. Il représente également l’équipe nationale du Japon, où il est un joueur clé. Avec son talent et son travail acharné, il est un modèle pour les jeunes passionnés de football.', 'téléphone : 01 23 45 67 89', 'asmonacoeur-kidstour/images/Takumi_Minamino.jpg'),
-(4, 'Akliouche', 'Maghnes', 'Maghnes Akliouche est un jeune footballeur français né le 25 février 2002 à Tremblay-en-France. Il évolue au poste de milieu offensif et est reconnu pour sa créativité, ses dribbles et sa vision du jeu. Formé à l’AS Monaco, il joue actuellement dans l’équipe professionnelle du club en Ligue 1. Akliouche est aussi pressenti comme un futur grand talent du football français. Avec son style élégant et son potentiel, il inspire les jeunes à croire en leurs rêves.', 'téléphone : 06 87 65 43 21', 'asmonacoeur-kidstour/images/Magnhes_Akliouche'),
-(5, 'Embolo', 'Breel', 'Breel Embolo est un footballeur suisse né le 14 février 1997 à Yaoundé, au Cameroun. Il joue comme attaquant et est connu pour sa puissance, sa vitesse et son sens du but. Embolo a évolué dans des clubs prestigieux comme Schalke 04 et le Borussia Mönchengladbach avant de rejoindre l’AS Monaco en France. Il représente l’équipe nationale de Suisse, avec laquelle il a marqué de nombreux buts importants. Son parcours impressionnant inspire les jeunes à travailler dur pour réussir dans le sport.', 'téléphone : 04 56 78 90 12', 'asmonacoeur-kidstour/images/Breel_Embolo.jpg');
+(1, 'Balogun', 'Folarin', 'Folarin Balogun est un jeune footballeur talentueux né le 3 juillet 2001 à New York, aux États-Unis. Il joue comme attaquant et est connu pour sa vitesse et ses nombreux buts. Balogun a grandi en Angleterre et a joué pour Arsenal avant de rejoindre l’AS Monaco en France. Il représente l’équipe nationale des États-Unis et est un espoir prometteur dans le monde du football. Avec son énergie et son talent, il inspire de nombreux jeunes à suivre leurs rêves dans le sport.', 'téléphone : 06 12 34 56 78', 'images/Folarin_Balogun.jpg'),
+(2, 'Golovin', 'Aleksandr', 'Aleksandr Golovin est un footballeur russe né le 30 mai 1996 à Kaltan, en Russie. Il joue au milieu de terrain et est connu pour sa technique, sa vision de jeu et ses passes précises. Golovin a commencé sa carrière en Russie avant de rejoindre l’AS Monaco, un club de Ligue 1 en France. Il a également représenté l’équipe nationale de Russie dans plusieurs compétitions internationales. Son travail acharné et son style de jeu créatif en font un joueur très apprécié des fans de football.', 'téléphone : 07 98 76 54 32', 'images/Aleksandr_Golovin.jpg'),
+(3, 'Minamino', 'Takumi', 'Takumi Minamino est un footballeur japonais né le 16 janvier 1995 à Izumisano, au Japon. Il joue comme attaquant ou milieu offensif et est connu pour sa rapidité, sa technique et son intelligence sur le terrain. Minamino a joué pour des clubs célèbres comme le Red Bull Salzburg et Liverpool avant de rejoindre l’AS Monaco en France. Il représente également l’équipe nationale du Japon, où il est un joueur clé. Avec son talent et son travail acharné, il est un modèle pour les jeunes passionnés de football.', 'téléphone : 01 23 45 67 89', 'images/Takumi_Minamino.jpg'),
+(4, 'Akliouche', 'Maghnes', 'Maghnes Akliouche est un jeune footballeur français né le 25 février 2002 à Tremblay-en-France. Il évolue au poste de milieu offensif et est reconnu pour sa créativité, ses dribbles et sa vision du jeu. Formé à l’AS Monaco, il joue actuellement dans l’équipe professionnelle du club en Ligue 1. Akliouche est aussi pressenti comme un futur grand talent du football français. Avec son style élégant et son potentiel, il inspire les jeunes à croire en leurs rêves.', 'téléphone : 06 87 65 43 21', 'images/Magnhes_Akliouche'),
+(5, 'Embolo', 'Breel', 'Breel Embolo est un footballeur suisse né le 14 février 1997 à Yaoundé, au Cameroun. Il joue comme attaquant et est connu pour sa puissance, sa vitesse et son sens du but. Embolo a évolué dans des clubs prestigieux comme Schalke 04 et le Borussia Mönchengladbach avant de rejoindre l’AS Monaco en France. Il représente l’équipe nationale de Suisse, avec laquelle il a marqué de nombreux buts importants. Son parcours impressionnant inspire les jeunes à travailler dur pour réussir dans le sport.', 'téléphone : 04 56 78 90 12', 'images/Breel_Embolo.jpg');
 
 -- --------------------------------------------------------
 
@@ -131,7 +131,7 @@ INSERT INTO `organise` (`id_organise`, `id_user`, `id_etape`) VALUES
 CREATE TABLE `participe` (
   `id_participe` int(11) NOT NULL,
   `id_user` int(11) DEFAULT NULL,
-  `id_evenement` int(11) DEFAULT NULL,
+  `id_etape` int(11) DEFAULT NULL,
   `nbr_enfant_participe` smallint(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -139,7 +139,7 @@ CREATE TABLE `participe` (
 -- Déchargement des données de la table `participe`
 --
 
-INSERT INTO `participe` (`id_participe`, `id_user`, `id_evenement`, `nbr_enfant_participe`) VALUES
+INSERT INTO `participe` (`id_participe`, `id_user`, `id_etape`, `nbr_enfant_participe`) VALUES
 (1, 6, 3, 2),
 (2, 7, 5, 3),
 (3, 8, 2, 1),
@@ -187,7 +187,7 @@ INSERT INTO `users` (`id_user`, `login_user`, `mdp_user`, `nom_user`, `prenom_us
 ALTER TABLE `anime`
   ADD PRIMARY KEY (`id_anime`),
   ADD KEY `id_invite` (`id_invite`),
-  ADD KEY `id_evenement` (`id_evenement`);
+  ADD KEY `id_evenement` (`id_etape`);
 
 --
 -- Index pour la table `etapes`
@@ -215,7 +215,7 @@ ALTER TABLE `organise`
 ALTER TABLE `participe`
   ADD PRIMARY KEY (`id_participe`),
   ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_evenement` (`id_evenement`);
+  ADD KEY `id_evenement` (`id_etape`);
 
 --
 -- Index pour la table `users`
@@ -266,7 +266,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `anime`
   ADD CONSTRAINT `anime_ibfk_1` FOREIGN KEY (`id_invite`) REFERENCES `invites` (`id_invite`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `anime_ibfk_2` FOREIGN KEY (`id_evenement`) REFERENCES `etapes` (`id_etape`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `anime_ibfk_2` FOREIGN KEY (`id_etape`) REFERENCES `etapes` (`id_etape`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `organise`
@@ -280,7 +280,7 @@ ALTER TABLE `organise`
 --
 ALTER TABLE `participe`
   ADD CONSTRAINT `participe_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `participe_ibfk_2` FOREIGN KEY (`id_evenement`) REFERENCES `etapes` (`id_etape`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `participe_ibfk_2` FOREIGN KEY (`id_etape`) REFERENCES `etapes` (`id_etape`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

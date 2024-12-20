e91378e94aee167b16d3c0f54e40db97f23ec904
CDPCharlotte2711 <madec.charlotte74@gmail.com>
2024-12-20 11:17:58 +0100

    Esthétique de la page admin

--- Composant/SelectEtapes.php
+++ Composant/SelectEtapes.php
@@ -1,8 +1,9 @@
 <div class="container py-4">
     <!-- Section Ordre -->
     <div class="mb-3">
+    <div class="bg-light p-4 rounded shadow-sm">
         <label for="ordre" class="form-label fw-bold">Ordre</label>
-        <select class="ordre" class="form-select">
+        <select class="ordre form-select">
             <option value="date_etape ASC, heure_etape ASC">Date (ascendant)</option>
             <option value="date_etape DESC, heure_etape DESC">Date (descendant)</option>
             <option value="nom_etape ASC">Nom (A -> Z)</option>
@@ -10,12 +11,12 @@
             <option value="ville_etape ASC">Ville (A -> Z)</option>
             <option value="ville_etape DESC">Ville (Z -> A)</option>
         </select>
-    </div>
+   
 
     <!-- Section Filtre -->
     <div class="mb-3">
         <label for="filtre" class="form-label fw-bold">Filtre</label>
-        <select class="filtre" class="form-select">
+        <select class="form-select filtre">
             <option value="nom_etape" data-type="text">Nom</option>
             <option value="ville_etape" data-type="text">Ville</option>
             <option value="date_etape" data-type="date">Date</option>
@@ -25,6 +26,7 @@
     <!-- Section Valeur -->
     <div class="mb-3">
         <label for="filtreValeur" class="form-label fw-bold">Valeur</label>
-        <input type="text" class="filtreValeur" class="form-control" placeholder="Entrez une valeur">
+        <input type="text" class="form-control filtreValeur" placeholder="Entrez une valeur">
+    </div>
     </div>
 </div>
--- Composant/templateFormSupprimerInvites.php
+++ Composant/templateFormSupprimerInvites.php
@@ -1,10 +1,10 @@
 <script id="templateFormSupprimerInvites" type="text/html">
-  <form action="AdminSupprimer.php" method="POST">
-    <select name="id_invite">
+  <form  action="AdminSupprimer.php" method="POST">
+    <select class="form-select" size="5" name="id_invite">
       {{ #.}}
       <option value="{{id_invite}}">{{nom_invite}}</option>
       {{ /. }}
     </select>
-    <input type="submit" value="Supprimer">
+    <input type="submit"  class="btn btn-danger shadow-sm"value="Supprimer">
   </form>
 </script>
